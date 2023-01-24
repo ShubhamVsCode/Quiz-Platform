9546c5befd400a82191ad9fdc2093da4a4c328ab
ShubhamVsCode <shubhamvscode@gmail.com>
2023-01-25 02:03:06 +0530

    Question Creation: We can create question direct by sending full options

--- controllers/quizController/question.controller.js
+++ controllers/quizController/question.controller.js
@@ -1,4 +1,5 @@
 const Question = require("../../model/quizModel/question.model");
+const Option = require("../../model/quizModel/option.model");
 const asyncHandler = require("../../services/asyncHandler");
 const CustomError = require("../../utils/customError");
 
@@ -8,6 +9,21 @@ const getQuestion = asyncHandler(async (req, res) => {
     if (!question_id) throw new CustomError("Question ID not provided", 401);
 
     const question = await Question.findById(question_id);
+
+    if (!question) throw new CustomError("Quiz is not available", 400);
+
+    res.status(200).json(question);
+  } catch (error) {
+    throw new CustomError(error.message, 500);
+  }
+});
+
+const getFullQuestion = asyncHandler(async (req, res) => {
+  try {
+    const { question_id } = req.params;
+    if (!question_id) throw new CustomError("Question ID not provided", 401);
+
+    const question = await Question.findById(question_id).populate("options");
 
     if (!question) throw new CustomError("Quiz is not available", 400);
 
@@ -31,9 +47,19 @@ const createQuestion = asyncHandler(async (req, res) => {
     if (options.length === 0)
       throw new CustomError("Options are required", 401);
 
+    const option1 = new Option(options[0]);
+    const option2 = new Option(options[1]);
+    const option3 = new Option(options[2]);
+    const option4 = new Option(options[3]);
+
+    await option1.save();
+    await option2.save();
+    await option3.save();
+    await option4.save();
+
     const question = await Question.create({
       question_text,
-      options,
+      options: [option1._id, option2._id, option3._id, option4._id],
       solution,
       question_image,
       question_difficulty,
@@ -93,6 +119,7 @@ const deleteQuestion = asyncHandler(async (req, res) => {
 
 module.exports = {
   getQuestion,
+  getFullQuestion,
   createQuestion,
   updateQuestion,
   deleteQuestion,
--- router/question.route.js
+++ router/question.route.js
@@ -4,13 +4,14 @@ const {
   createQuestion,
   updateQuestion,
   deleteQuestion,
+  getFullQuestion,
 } = require("../controllers/quizController/question.controller");
 const { isLoggedIn } = require("../middleware/auth.middleware");
 
 const questionRoute = express.Router();
 
-questionRoute.get("/:question_id", isLoggedIn, getQuestion);
-questionRoute.post("/", isLoggedIn, createQuestion);
+questionRoute.get("/:question_id", isLoggedIn, getFullQuestion);
+questionRoute.post("/", createQuestion); //TODO: isLoggedIn is not there please keep that for production
 questionRoute.put("/:question_id", isLoggedIn, updateQuestion);
 questionRoute.delete("/:question_id", isLoggedIn, deleteQuestion);
 
